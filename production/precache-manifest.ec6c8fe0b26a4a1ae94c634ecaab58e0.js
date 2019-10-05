506b52c13904f007d2b87e685a27964a3147fdba
Mark11101 <r-io-home@ya.ru>
2019-10-05 23:18:27 +0400

    final

---
+++ production/precache-manifest.ec6c8fe0b26a4a1ae94c634ecaab58e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7af30d405ac1199986e97f5efe5b44e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5366f1552960893acab3",
+    "url": "/static/css/main.b76c2beb.chunk.css"
+  },
+  {
+    "revision": "d9ef1a7041f6096d5051",
+    "url": "/static/js/2.4f4395e5.chunk.js"
+  },
+  {
+    "revision": "5366f1552960893acab3",
+    "url": "/static/js/main.7821987a.chunk.js"
+  },
+  {
+    "revision": "d9c1e15924498ac05f53",
+    "url": "/static/js/runtime-main.b3ce2a64.js"
+  },
+  {
+    "revision": "e0f35b743dc7a1f80b3e36ae64aaf55b",
+    "url": "/static/media/office.e0f35b74.jpg"
+  }
+]);
--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,6 +4,9 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+import firebase from 'firebase/app';
 
 interface propsType {
     employees: any;
@@ -14,26 +17,73 @@ interface propsType {
     }
 }
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzQfZE1llM6SshUMq3HuDZ-yUbJvQ4LV0",
+    authDomain: "myemployees-9a164.firebaseapp.com",
+    databaseURL: "https://myemployees-9a164.firebaseio.com",
+    storageBucket: "myemployees-9a164.appspot.com",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var storage = require('@google-cloud/storage');
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const fileStorage = firebase.storage();
+// Points to the root reference
+var storageRef = firebase.storage().ref();
+
+// Points to 'images'
+var imagesRef = storageRef.child('image');
+
+// Points to 'images/space.jpg'
+// Note that you can use variables to create child values
+var fileName = 'Roman.jpg';
+var spaceRef = imagesRef.child(fileName);
+
+// File path is 'images/space.jpg'
+var path = spaceRef.fullPath;
+
+// File name is 'space.jpg'
+var name = spaceRef.name;
+
+// Points to 'images'
+var imagesRef = spaceRef.parent;
+
+
 const Dashboard = (props: propsType): object => {
 
     const { employees, auth, users } = props;
 
+    const [photo, setPhoto] = useState(null);
+
     if (!auth.uid) {
         return <Redirect to='/signin'/>
     }
+
+    const fileSelectedHandler = (e: any) => {
+        setPhoto(e.target.files[0]);
+    };
+
+    const fileUploadHandler = () => {
+        console.log(photo);
+    };
 
     return (
         <div className="dashboard container">
             <div className="col s12 m6">
                 <div className="card-columns">
                     <EmployeeSummary employees={employees} auth={auth} users={users}/>
+                    <input type="file" onChange={fileSelectedHandler}/>
+                    <button onClick={fileUploadHandler}>Upload</button>
                 </div>
             </div>
         </div>
     )
 };
 
-const mapStateToProps = (state: any): object => {console.log(state);
+const mapStateToProps = (state: any): object => {
     return {
         employees: state.firestore.ordered.employees,
         auth: state.firebase.auth,
